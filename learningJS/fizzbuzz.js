2c959235770c40493b82048ffa185f78420895c2
Christian Edwards <49890784+cheddy1@users.noreply.github.com>
2021-03-31 11:02:33 -0500

    EECS 368 HW 1

--- learningJS/fizzbuzz.js
+++ learningJS/fizzbuzz.js
@@ -1,3 +1,6 @@
+/*Implement fizz-buzz in JavaScript
+The program should run for the numbers 1 to 100. There should be spaces (or newlines) between the numbers and words.*/
+
 for (let i = 1; i <= 100; i++) {
     if ((i % 5 == 0) && (i % 3 == 0)) {
         console.log('fizzbuzz')
