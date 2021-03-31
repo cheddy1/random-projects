b2e18fb85cc354eff29f9c0b47eff8772699bb0e
Christian Edwards <49890784+cheddy1@users.noreply.github.com>
2021-03-31 11:04:28 -0500

    EECS 368 HW 4

--- learningJS/promiseCount.js
+++ learningJS/promiseCount.js
@@ -1,3 +1,8 @@
+/*
+Write a JavaScript function called timings that uses Promises to take an array of times, and then print the numbers after that number of seconds. 
+The timings function should return a Promise that resolves after the final print.
+*/
+
 function timings(timeArray) {
     return Promise.all(timeArray.map(timing => new Promise((resolve, reject) => {
         setTimeout(() => { resolve(console.log(timing)) }, timing * 1000)
